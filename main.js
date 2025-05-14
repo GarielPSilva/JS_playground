ed07215bd1aefce790411c57395cc78e45247f94
Gabriel Peres <bielpps@gmail.com>
2025-05-14 18:45:31 -0300

    first js code

---
+++ main.js
@@ -0,0 +1,9 @@
+const firstName = 'Gabriel'
+const lastName = 'Peres'
+
+console.log(`My name is ${firstName} ${lastName}`)
+
+const age = '18'
+let birthday = '05/22'
+
+console.log(`I'm ${age}and my birthday will be in ${birthday}`)
