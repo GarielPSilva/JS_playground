76b6eceab24ec384ae26b2e158adb55f3a8c4d90
Gabriel Peres <bielpps@gmail.com>
2025-06-03 19:24:56 -0300

    learning loop

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-const todos = [
-    {
-        id: 1,
-        description: "Learn a new thing",
-        isCompleted: true,
-    },
-    {
-        id: 2,
-        description: "Studyin JavaScript",
-        isCompleted: true,
-    },
-    {
-        id: 3,
-        description: "Play videogames",
-        isCompleted: false,
-    },
-];
-
-const todosJSON = JSON.stringify(todos);
-const todosList = JSON.parse(todosJSON);
-// sempre que mandar para um servidor/whenever you send to a server
-console.log(todosList);
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+} 
