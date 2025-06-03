ffc66fa00411bac1a1d73d95b34935c72ebb01fd
Gabriel Peres <bielpps@gmail.com>
2025-06-03 19:41:37 -0300

    sports loop

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-for (let index = 0; index < 10; index++) {
-    console.log(index);
-} 
+const sports = ["Bascketball", "Volleyball", "Soccer"];
+
+for (let i = 0; i < sports.length; i++ ) {
+    console.log(sports[i]);
+}
+
+sports.forEach(function(sports) {
+    console.log(sports);
+}) 
