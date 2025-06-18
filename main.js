97e5ed2a94dd188e1a259a85c34c8b74d1293e4c
Gabriel Peres <bielpps@gmail.com>
2025-06-18 17:57:38 -0300

    ternary operator

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-const sum = 2 + 5;
-const sum2 = 3 + 1;
+const sum = 3 + 3
 
-if (sum === 7 && sum2 === 4) {
-    console.log("sum is 7 and sum2 is 4")
-}
+let number = sum === 2 ? 2 : 6
 
-if (sum === 7 || sum2 === 4) {
-    console.log("sum is 7 or sum2 is 4")
-}
+console.log(number);
