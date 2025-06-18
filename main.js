da8cd9582797c2afc1068b6c7b06b83bef1c2776
Gabriel Peres <bielpps@gmail.com>
2025-06-18 17:55:50 -0300

    (&&) and & or (||)

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const sum = 2 + 5;
+const sum2 = 3 + 1;
 
-if (sum === 7){
-    console.log("Sum is 7")
-} else {
-    console.log("Sum is not 7")
+if (sum === 7 && sum2 === 4) {
+    console.log("sum is 7 and sum2 is 4")
+}
+
+if (sum === 7 || sum2 === 4) {
+    console.log("sum is 7 or sum2 is 4")
 }
